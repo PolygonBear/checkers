28b9a77ae3566f3c35014053045669b2799084d4
? <Nikita@DESKTOP-2LF2544>
2022-06-18 03:17:36 +0600

    Я идиот

--- Checkers/Checkers/main.cpp
+++ Checkers/Checkers/main.cpp
@@ -24,28 +24,27 @@ char FieldAsSymbol(const Field field)
 
 int main()
 {
-    std::cout << "Select side: 1. white, 2. black: ";
-    int side = 0;
-
     Field playerSide = eEmpty;
 
-    while (std::cin >> side)
+    while (true)
     {
-        if (side == 1)
+        std::cout << "Select side: 1. white, 2. black: ";
+
+        char side = 0;
+        std::cin >> side;
+
+        if (side == '1')
         {
             playerSide = eWhiteChess;
             break;
         }
-        else if (side == 2)
+        else if (side == '2')
         {
             playerSide = eBlackChess;
             break;
         }
         else
-        {
-            std::cout << "Your enter invalid choise!";
-            return 1;
-        }
+            std::cout << "Your enter invalid choise!" << std::endl;
     }
 
     Field chessField[8][8];
@@ -54,37 +53,37 @@ int main()
      A     B     C     D     E     F     G     H
   #################################################
   #     #     #     #     #     #     #     #     #
-8 #     #     #     #     #     #     #     #     #
+8 #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #
   #     #     #     #     #     #     #     #     #
   #################################################
   #     #     #     #     #     #     #     #     #
-7 #     #     #     #     #     #     #     #     #
+7 #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #
   #     #     #     #     #     #     #     #     #
   #################################################
   #     #     #     #     #     #     #     #     #
-6 #     #     #     #     #     #     #     #     #
+6 #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #
   #     #     #     #     #     #     #     #     #
   #################################################
   #     #     #     #     #     #     #     #     #
-5 #     #     #     #     #     #     #     #     #
+5 #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #
   #     #     #     #     #     #     #     #     #
   #################################################
   #     #     #     #     #     #     #     #     #
-4 #     #     #     #     #     #     #     #     #
+4 #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #
   #     #     #     #     #     #     #     #     #
   #################################################
   #     #     #     #     #     #     #     #     #
-3 #     #     #     #     #     #     #     #     #
+3 #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #
   #     #     #     #     #     #     #     #     #
   #################################################
   #     #     #     #     #     #     #     #     #
-2 #     #     #     #     #     #     #     #     #
+2 #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #
   #     #     #     #     #     #     #     #     #
   #################################################
   #     #     #     #     #     #     #     #     #
-1 #     #     #     #     #     #     #     #     #
+1 #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #  {}  #
   #     #     #     #     #     #     #     #     #
-  #################################################)") << std::endl << std::endl;
+  #################################################)", FieldAsSymbol(chessField[0][0]), FieldAsSymbol(chessField[0][1]), FieldAsSymbol(chessField[0][2]), FieldAsSymbol(chessField[0][3]), FieldAsSymbol(chessField[0][4]), FieldAsSymbol(chessField[0][5]), FieldAsSymbol(chessField[0][6]), FieldAsSymbol(chessField[0][7]), FieldAsSymbol(chessField[1][0]), FieldAsSymbol(chessField[1][1]), FieldAsSymbol(chessField[1][2]), FieldAsSymbol(chessField[1][3]), FieldAsSymbol(chessField[1][4]), FieldAsSymbol(chessField[1][5]), FieldAsSymbol(chessField[1][6]), FieldAsSymbol(chessField[1][7]), FieldAsSymbol(chessField[2][0]), FieldAsSymbol(chessField[2][1]), FieldAsSymbol(chessField[2][2]), FieldAsSymbol(chessField[2][3]), FieldAsSymbol(chessField[2][4]), FieldAsSymbol(chessField[2][5]), FieldAsSymbol(chessField[2][6]), FieldAsSymbol(chessField[2][7]), FieldAsSymbol(chessField[3][0]), FieldAsSymbol(chessField[3][1]), FieldAsSymbol(chessField[3][2]), FieldAsSymbol(chessField[3][3]), FieldAsSymbol(chessField[3][4]), FieldAsSymbol(chessField[3][5]), FieldAsSymbol(chessField[3][6]), FieldAsSymbol(chessField[3][7]), FieldAsSymbol(chessField[4][0]), FieldAsSymbol(chessField[4][1]), FieldAsSymbol(chessField[4][2]), FieldAsSymbol(chessField[4][3]), FieldAsSymbol(chessField[4][4]), FieldAsSymbol(chessField[4][5]), FieldAsSymbol(chessField[4][6]), FieldAsSymbol(chessField[4][7]), FieldAsSymbol(chessField[5][0]), FieldAsSymbol(chessField[5][1]), FieldAsSymbol(chessField[5][2]), FieldAsSymbol(chessField[5][3]), FieldAsSymbol(chessField[5][4]), FieldAsSymbol(chessField[5][5]), FieldAsSymbol(chessField[5][6]), FieldAsSymbol(chessField[5][7]), FieldAsSymbol(chessField[6][0]), FieldAsSymbol(chessField[6][1]), FieldAsSymbol(chessField[6][2]), FieldAsSymbol(chessField[6][3]), FieldAsSymbol(chessField[6][4]), FieldAsSymbol(chessField[6][5]), FieldAsSymbol(chessField[6][6]), FieldAsSymbol(chessField[6][7]), FieldAsSymbol(chessField[7][0]), FieldAsSymbol(chessField[7][1]), FieldAsSymbol(chessField[7][2]), FieldAsSymbol(chessField[7][3]), FieldAsSymbol(chessField[7][4]), FieldAsSymbol(chessField[7][5]), FieldAsSymbol(chessField[7][6]), FieldAsSymbol(chessField[7][7])) << std::endl << std::endl;
 
     while (true)
     {
