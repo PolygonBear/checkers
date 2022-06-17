20ceaf5cf7f3921dffc9002cd1b7610ba7769c30
PolygonBear <Semenyaka.maxim@yandex.ru>
2022-06-17 23:52:33 +0300

    add select player side

--- Checkers/Checkers/main.cpp
+++ Checkers/Checkers/main.cpp
@@ -10,6 +10,21 @@ enum Field
 
 int main()
 {
+    std::cout << "Select side: 1. white, 2. black: ";
+    int side = 0;
+    std::cin >> side;
+
+    Field playerSide = eEmpty;
+
+    if (side == 1)
+    {
+        playerSide = eWhiteChess;
+    }
+    else if (side == 2)
+    {
+        playerSide = eBlackChess;
+    }
+
     Field chessField[8][8];
 
     while (true)
